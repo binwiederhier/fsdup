3dfd9fc9e2f8c13d40aebde8c7025bdffe9cebe5
Philipp Heckel <pheckel@datto.com>
2019-05-28 16:28:00 -0400

    Simplify writer

--- map.go
+++ map.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"github.com/samalba/buse-go/buse"
 	"log"
 	"os"
--- store_file.go
+++ store_file.go
@@ -22,24 +22,16 @@ func NewFileStore(root string) *fileChunkStore {
 
 func (idx *fileChunkStore) WriteChunk(chunk *chunk) error {
 	if _, ok := idx.chunkMap[chunk.ChecksumString()]; !ok {
-		if err := idx.writeChunkFile(chunk); err != nil {
-			return err
+		chunkFile := fmt.Sprintf("%s/%x", idx.root, chunk.Checksum())
+
+		if _, err := os.Stat(chunkFile); err != nil {
+			err = ioutil.WriteFile(chunkFile, chunk.Data(), 0666)
+			if err != nil {
+				return err
+			}
 		}
 
 		idx.chunkMap[chunk.ChecksumString()] = true
-	}
-
-	return nil
-}
-
-func (idx *fileChunkStore) writeChunkFile(chunk *chunk) error {
-	chunkFile := fmt.Sprintf("%s/%x", idx.root, chunk.Checksum())
-
-	if _, err := os.Stat(chunkFile); err != nil {
-		err = ioutil.WriteFile(chunkFile, chunk.Data(), 0666)
-		if err != nil {
-			return err
-		}
 	}
 
 	return nil
